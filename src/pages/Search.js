78a634a5816a9685dea8ea13e56c09577464da0a
Andre <andre360.ars@gmail.com>
2022-03-07 22:10:37 -0300

    Requisito 5 - adiona campo de busca pelo nome do artista ou banda

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,48 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      arstistName: '',
+      disable: true,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+      disable: value.length < 2,
+    });
+  }
+
   render() {
+    const { state: { arstistName, disable }, onInputChange } = this;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>searchPage</p>
+        <div className="SearchBar">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="arstistName"
+            value={ arstistName }
+            onChange={ onInputChange }
+          />
+          <button
+            disabled={ disable }
+            type="button"
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+
+          </button>
+        </div>
       </div>
     );
   }
