ba9e44c0feb54dd346957fc65b3ec79a2a816810
Andre <andre360.ars@gmail.com>
2022-03-07 21:34:12 -0300

    Requisito 4 - cria os links de navegaçao, entre os componentes: Search, Favorites e Profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -36,7 +37,11 @@ class Header extends Component {
             ? (<span data-testid="header-user-name">{userName}</span>)
             : (<Loading />)
         }
-
+        <nav>
+          <Link data-testid="link-to-search" to="/search">Search</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+          <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+        </nav>
       </header>
     );
   }
