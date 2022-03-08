9c96f2929ed070628a1e99859170b45e2a114d57
Andre <andre360.ars@gmail.com>
2022-03-08 12:16:10 -0300

    Requisito 6 - adiciona evento de click no botao de busca, que renderiza os albuns, do artista pesquisado, e retorna mensagem de erro quando nao encontra

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends Component {
   constructor(props) {
@@ -11,6 +14,8 @@ class Search extends Component {
     };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.fetchArtist = this.fetchArtist.bind(this);
+    this.albumRender = this.albumRender.bind(this);
   }
 
   onInputChange({ target }) {
@@ -22,12 +27,58 @@ class Search extends Component {
     });
   }
 
+  fetchArtist() {
+    const { arstistName } = this.state;
+    this.setState({ loading: true, artist: arstistName }, async () => {
+      const albumList = await searchAlbumsAPI(arstistName);
+
+      this.setState({
+        arstistName: '',
+        disable: true,
+        albuns: albumList.length > 0 ? albumList : 'not found',
+        loading: false,
+      });
+    });
+  }
+
+  albumRender() {
+    const { state: { loading, albuns, artist } } = this;
+    if (loading && !albuns) {
+      return <Loading />;
+    }
+
+    if (Array.isArray(albuns)) {
+      const albumList = albuns
+        .map(({ artistName, collectionId, collectionName, artworkUrl100 }) => (
+          <Link
+            to={ `/album/${collectionId}` }
+            data-testid={ `link-to-album-${collectionId}` }
+            key={ collectionId }
+          >
+            <img src={ artworkUrl100 } alt={ collectionName } />
+            <h3>{ collectionName }</h3>
+            <h4>{ artistName }</h4>
+          </Link>
+        ));
+      return (
+        <>
+          <h2>{ `Resultado de álbuns de: ${artist}` }</h2>
+          {albumList}
+        </>
+      );
+    }
+
+    if (albuns === 'not found') return <h2>Nenhum álbum foi encontrado</h2>;
+  }
+
   render() {
-    const { state: { arstistName, disable }, onInputChange } = this;
+    const { state: { arstistName, disable },
+      onInputChange, fetchArtist, albumRender } = this;
+
     return (
       <div data-testid="page-search">
         <Header />
-        <div className="SearchBar">
+        <form className="SearchBar">
           <input
             type="text"
             data-testid="search-artist-input"
@@ -39,11 +90,12 @@ class Search extends Component {
             disabled={ disable }
             type="button"
             data-testid="search-artist-button"
+            onClick={ fetchArtist }
           >
             Pesquisar
-
           </button>
-        </div>
+        </form>
+        {albumRender()}
       </div>
     );
   }
